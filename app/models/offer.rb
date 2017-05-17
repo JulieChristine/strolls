438632a3357c1d72820f657538de6ef66759a8dd
JulieChristine <julie.wolsak@gmail.com>
2017-05-17 13:38:26 +0800

    add test for savings and validity in Offer table

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,3 +1,11 @@
 class Offer < ActiveRecord::Base
   validates :name, :presence => true
 end
+
+class Offer < ActiveRecord::Base
+  validates :savings, :presence => true
+end
+
+class Offer < ActiveRecord::Base
+  validates :validity, :presence => true
+end
--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 describe Offer do
   it { should validate_presence_of :name }
+  it { should validate_presence_of :savings }
+  it { should validate_presence_of :validity }
 end
