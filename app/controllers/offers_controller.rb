bda53dcd59dcfba5046406fdd6358f78db2a368a
JulieChristine <julie.wolsak@gmail.com>
2017-05-19 14:46:53 +0800

    add create new offer functionality

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -14,4 +14,18 @@ class OffersController < ApplicationController
     render :new
   end
 
+  def create
+    @offer = Offer.new(offer_params)
+    if @offer.save
+      redirect_to offers_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def offer_params
+      params.require(:offer).permit(:name, :validity, :savings)
+    end
+
 end
