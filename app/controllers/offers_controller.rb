1c43196644069b142385be662ec4ba99c876d7ba
JulieChristine <julie.wolsak@gmail.com>
2017-05-22 12:39:16 +0800

    add offers to shops with form, add in controller, display offers in shops

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,6 +1,22 @@
 class OffersController < ApplicationController
+  def create
+    @shop = Shop.find(params[:shop_id])
+    @offer = @shop.offers.new(offer_params)
+
+    if @offer.save
+      redirect_to shop_path(@offer.shop)
+    else
+      render :new
+    end
+  end
+
   def new
     @shop = Shop.find(params[:shop_id])
     @offer = @shop.offers.new
   end
+
+  private
+    def offer_params
+      params.require(:offer).permit(:name, :savings, :validity)
+    end
 end
