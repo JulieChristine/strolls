07fc06e69b1955624a9588117730082bd750c6c9
JulieChristine <julie.wolsak@gmail.com>
2017-05-22 15:29:02 +0800

    add flash messages in shop controller

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -17,7 +17,8 @@ class ShopsController < ApplicationController
   def create
     @shop = Shop.new(shop_params)
     if @shop.save
-      redirect_to shops_path
+      flash[:notice] = "Shop successfully added!"
+      redirect_to shops_path(@shop)
     else
       render :new
     end
@@ -31,6 +32,7 @@ class ShopsController < ApplicationController
   def destroy
     @shop = Shop.find(params[:id])
     @shop.destroy
+    flash[:notice] = "The shop has been deleted"
     redirect_to shops_path
   end
 
